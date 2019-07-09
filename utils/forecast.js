e6169e82c6e537d28e4ed2f04e13547cb9fe099f
Vaibhav Gurjar <vaibhav.gurjar@gmail.com>
2019-07-09 17:23:27 +0530

    Misc changes

--- utils/forecast.js
+++ utils/forecast.js
@@ -7,14 +7,14 @@ const forecast = (latitude, longitude,callback) => {
     const url = 'https://api.darksky.net/forecast/9cfb32de0da99c1ebc5eb2cde71b311e/'+latitude+','+longitude+'?units=si'
 
     // Call to 'request API' passing in URL
-    request({ url: url, json: true }, (error, response) => {
+    request({ url, json: true }, (error, {body}) => {
 
         if (error) {
             callback('Unable to connect to location services!', undefined)
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to find location, Try again',undefined)
         } else {
-            callback(undefined, response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degress out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
